e58f963395cb851bedfe55ac2e008f2faba80d09
masakazu yamaya <yamaya01masakazu04@gmail.com>
2016-09-06 21:01:31 +0900

    Infel yor ARAKAWA! リセット、１つ戻る完成

--- src/app.js
+++ src/app.js
@@ -18,6 +18,7 @@ var level_map = [
   [1, 0, 0, 1, 1, 1, 1],
   [1, 1, 1, 1, 1, 1, 1]
 ];
+
 /*
 var level02 = [
   [1, 1, 1, 1, 1, 1, 1],
@@ -34,7 +35,9 @@ var kazu = 1;
 var playerPosition; //マップ内のプレイやの位置(ｘ、ｙ)を保持する
 var playerSprite; //プレイヤーのスプライト
 var cratesArray = []; //配置した木箱のスプライトを配列に保持する
-var cratesArray_map = []; //バックで戻る方の配列
+var cratesArray_map = []; //バックで戻る方の木箱配列
+var backlevel_map = []; //バックで戻る時の配列
+var def_crates = []; //初期する木箱配列
 
 var startTouch;
 var endTouch;
@@ -87,6 +90,7 @@ var gameLayer = cc.Layer.extend({
 
     for (i = 0; i < 7; i++) {　　　　　　
       cratesArray[i] = [];　 //配列オブジェクトの生成
+      def_crates[i] = []; //リセット用生成
       for (j = 0; j < 7; j++) {
         switch (/*("*/level/*0" + kazu)*/[i][j]) {
           case 2:
@@ -103,6 +107,8 @@ var gameLayer = cc.Layer.extend({
               y: i
             };　　　　　　　　　　　　
             cratesArray[i][j] = null;　 //playerがいるので、その場所には木箱はないのでnullを代入する
+            var def = cratesArray[i][j];
+            def_crates[i][j] = def;
             break;
           case 3:
           case 5:
@@ -112,10 +118,15 @@ var gameLayer = cc.Layer.extend({
             this.addChild(crateSprite);
             cratesArray[i][j] = crateSprite;//(i,j)の位置にcrateSpriteを入れる
 
+            var def = cratesArray[i][j];
+            def_crates[i][j] = def;
 
             break;
           default:
             cratesArray[i][j] = null;//木箱のコード以外の場合は、その場所に木箱がない値としてnullを代入する
+
+            var def = cratesArray[i][j];
+            def_crates[i][j] = def;
             break;
         }
       }
@@ -134,6 +145,20 @@ var gameLayer = cc.Layer.extend({
     });*/
     //return true;
     cc.eventManager.addListener(listener, this);
+    //-------------↓キーボード↓-------------
+    cc.eventManager.addListener({
+      event: cc.EventListener.KEYBOARD,
+      onKeyPressed: function(keyCode, event) {
+        if(keyCode == 82){
+          console.log("r押した" );
+          resetmap();
+        }
+        if(keyCode == 66){
+          console.log("b押した" );
+          backmap();
+        }
+      },
+    }, this);
   },
 });
 
@@ -147,7 +172,6 @@ return true;
 onTouchEnded:function(touch, event){
 endTouch = touch.getLocation();
 swipeDirection();
-keybord();
 }
 });
 //スワイプ方向を検出する処理
@@ -185,97 +209,21 @@ function swipeDirection(){
         }
     }
 }
-//----------------【↓キーボード↓】------------------
-function keybord(){
-
-  if ('keyboard' in cc.sys.capabilities) {
-cc.eventManager.addListener({
-  event: cc.EventListener.KEYBOARD,
-  onKeyPressed: function(key, event) {
-    switch (key) {
-      //...
-      case 66:
-      for (i = 0; i < 7; i++) {　　　　　　
-        for (j = 0; j < 7; j++) {
-
-          var mapsuu = 0
-
-          mapsuu = level_map[i][j];
-          level[i][j] = mapsuu;
-
-          //★★★switchでlevelをlevel_mapに更新(1つ前に戻る)
-              switch (level[i][j]) {
-                case 2:
-                  break;
-                case 4:
-                case 6:
-                  playerSprite.setPosition(165 + 25 * j, 185 - 25 * i);
-                  playerPosition = {
-
-                    x: j,
-                    y: i
-                  };　　　　　　　　　　　　
-                  cratesArray_map[i][j] = null;　 //playerがいるので、その場所には木箱はないのでnullを代入する
-                  break;
-                case 3:
-                case 5:
-                var crateSprite = cc.Sprite.create(cache.getSpriteFrame("crate.png"));
-                  crateSprite.setPosition(165 + 25 * j, 185 - 25 * i);
-                  cratesArray_map[i][j] = crateSprite;//(i,j)の位置にcrateSpriteを入れる
-
-                  break;
-                default:
-                  cratesArray_map[i][j] = null;//木箱のコード以外の場合は、その場所に木箱がない値としてnullを代入する
-                  break;
-              }
-        }
-      }
-    }
-  },
-  /*
-  onKeyReleased: function(key, event) {
-    //...
-  },*/
-}, this);
-}
-
-}
-
-//----------------【↑キーボード↑】------------------
 
 function move(deltaX,deltaY){ //deltaはmoveの中身
   //var size = cc.director.getWinSize();
-    //-------------------【↓バックで戻るやつ↓】--------------------------
-      for (i = 0; i < 7; i++) {　　　　　　
-        cratesArray_map[i] = [];　 //配列オブジェクトの生成
-        for (j = 0; j < 7; j++) {
-          switch (/*("*/level/*0" + kazu)*/[i][j]) {
-            case 2:
-              break;
-            case 4:
-            case 6:
-              playerSprite.setPosition(165 + 25 * j, 185 - 25 * i);
-              playerPosition = {
-                x: j,
-                y: i
-              };　　　　　　　　　　　　
-              cratesArray_map[i][j] = null;　 //playerがいるので、その場所には木箱はないのでnullを代入する
-              break;
-            case 3:
-            case 5:
-            var crateSprite = cc.Sprite.create(cache.getSpriteFrame("crate.png"));
-              crateSprite.setPosition(165 + 25 * j, 185 - 25 * i);
-              cratesArray_map[i][j] = crateSprite;//(i,j)の位置にcrateSpriteを入れる
-
-              break;
-            default:
-              cratesArray_map[i][j] = null;//木箱のコード以外の場合は、その場所に木箱がない値としてnullを代入する
-              break;
-          }
+    //-------------------【↓１個まえに戻るためのバックアップ↓】--------------------------
+    for (var i = 0; i < 7; i++){
+          cratesArray_map[i] = [];
+          backlevel_map[i] = [];
+        for (var j = 0; j < 7; j++){
+          var back1 = level[i][j];
+                 backlevel_map[i][j] = back1;
+          var back2 = cratesArray[i][j];
+                 cratesArray_map[i][j] = back2;
         }
       }
-      console.log("anasuu" + anasuu );
-      //-------------------【↑バックで戻るやつ↑】--------------------------
+      //-------------------【↑バックアップ↑】--------------------------
 switch(/*("*/level/*0" + kazu)*/[playerPosition.y+deltaY][playerPosition.x+deltaX]){ //もしもプレイヤーの場所+move=移動先がcaseの数字なら
     case 0:
     case 2: //ただの移動
@@ -326,3 +274,86 @@ function clearGame(){
 
 }
 */
+
+
+function resetmap(){
+  for (i = 0; i < 7; i++) {　　　　　　
+    for (j = 0; j < 7; j++) {
+
+      var mapsuu = level_map[i][j];
+      level[i][j] = mapsuu;
+
+      //★★★switchでlevelをlevel_mapに更新(1つ前に戻る)
+          switch (level[i][j]) {
+            case 2:　//穴
+              break;
+            case 4: //プレイヤー
+            case 6:
+              playerSprite.setPosition(165 + 25 * j, 185 - 25 * i);
+              playerPosition = {
+
+                x: j,
+                y: i
+              };　　　　　　　　　　　　
+              var def = def_crates[i][j];
+              cratesArray[i][j] = def;
+              break;
+            case 3: //木箱
+            case 5:
+            var def = def_crates[i][j];
+            cratesArray[i][j] = def;
+              var crateSprite = cratesArray[i][j];
+              crateSprite.setPosition(165 + 25 * j, 185 - 25 * i);
+
+              break;
+            default:
+            var def = def_crates[i][j];
+            cratesArray[i][j] = def;
+              break;
+          }
+    }
+  }
+}
+
+function backmap() {
+
+  for (i = 0; i < 7; i++) {　　　　　　
+    for (j = 0; j < 7; j++) {
+
+      var mapsuu = backlevel_map[i][j];
+      level[i][j] = mapsuu;
+
+      //★★★switchでlevelをlevel_mapに更新(1つ前に戻る)
+          switch (level[i][j]) {
+            case 2:　//穴
+              break;
+            case 4: //プレイヤー
+            case 6:
+              playerSprite.setPosition(165 + 25 * j, 185 - 25 * i);
+              playerPosition = {
+
+                x: j,
+                y: i
+              };　　　　　　　　　　　　
+              var back02 = cratesArray_map[i][j];
+              cratesArray[i][j] = back02;
+              break;
+            case 3: //木箱
+            case 5:
+              var back02 = cratesArray_map[i][j];
+              cratesArray[i][j] = back02;
+              var crateSprite = cratesArray[i][j];
+              crateSprite.setPosition(165 + 25 * j, 185 - 25 * i);
+
+              break;
+            default:
+              var back02 = cratesArray_map[i][j];
+              cratesArray[i][j] = back02;
+              break;
+          }
+    }
+  }
+
+}
+
+//----------------【↑キーボード↑】------------------
